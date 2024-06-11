e336b00e272bd2891e069ac350d18b34acb77fc7
Sterling Hanenkamp <sterling@hanenkamp.com>
2024-06-11 14:42:26 -0500

    fix: linter is dumb on this one

--- pkg/ref/filter_test.go
+++ pkg/ref/filter_test.go
@@ -87,44 +87,41 @@ func TestCanon_Filtered_Overlaps(t *testing.T) {
 	}
 
 	for _, v := range variants {
-		TestCanon_Filtered_Overlap := func(t *testing.T) {
-			t.Parallel()
-
-			c, err := ref.Canonical.Filtered(v...)
-			assert.NoError(t, err)
-			assert.NotNil(t, c)
-
-			b, err := c.Book("Daniel")
-			assert.NoError(t, err)
-			assert.NotNil(t, b)
-
-			ps, err := c.Category("Apocalyptic")
-			assert.NoError(t, err)
-
-			toRefs := make([]string, len(ps))
-			for i, p := range ps {
-				toRefs[i], err = p.Ref.CompactRef()
-				assert.NoError(t, err)
-			}
-
-			assert.Equal(t, []string{
-				"Daniel 8-12",
-				"Revelation",
-				"Amos 7:1-9",
-				"Amos 8:1-13",
-				"Isaiah 24-27",
-				"Isaiah 33",
-				"Isaiah 55-56",
-				"Jeremiah 1:11-16",
-				"Ezekiel 38-39",
-				"Zechariah 9-14",
-				"Joel",
-			}, toRefs)
-		}
-
 		t.Run(
 			fmt.Sprintf("Test %q merged with %q", v[0], v[1]),
-			TestCanon_Filtered_Overlap,
-		)
+			func(t *testing.T) {
+				t.Parallel()
+
+				c, err := ref.Canonical.Filtered(v...)
+				assert.NoError(t, err)
+				assert.NotNil(t, c)
+
+				b, err := c.Book("Daniel")
+				assert.NoError(t, err)
+				assert.NotNil(t, b)
+
+				ps, err := c.Category("Apocalyptic")
+				assert.NoError(t, err)
+
+				toRefs := make([]string, len(ps))
+				for i, p := range ps {
+					toRefs[i], err = p.Ref.CompactRef()
+					assert.NoError(t, err)
+				}
+
+				assert.Equal(t, []string{
+					"Daniel 8-12",
+					"Revelation",
+					"Amos 7:1-9",
+					"Amos 8:1-13",
+					"Isaiah 24-27",
+					"Isaiah 33",
+					"Isaiah 55-56",
+					"Jeremiah 1:11-16",
+					"Ezekiel 38-39",
+					"Zechariah 9-14",
+					"Joel",
+				}, toRefs)
+			})
 	}
 }
